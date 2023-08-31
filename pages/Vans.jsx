e75e65b91b88a41ff1f4bc466170062bd8a61dae
bruxo <diogo.stein@outlook.com.br>
2023-08-31 17:52:43 -0300

    some css tweaks

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Vans() {
   const [vans, setVans] = useState([]);
@@ -11,22 +12,22 @@ function Vans() {
 
   const vanElements = vans.map((van) => (
     <div key={van.id} className="van-tile">
-      <img src={van.imageUrl} />
-      <div className="van-info">
-        <h3>{van.name}</h3>
-        <p>
-          ${van.price}
-          <span>/day</span>
-        </p>
-      </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
     </div>
   ));
 
-  console.log(vans);
-
   return (
-    <div className="van-list-container">
+    <div className="van-list-container wrapper">
       <h1>Explore our van options</h1>
       <div className="van-list">{vanElements}</div>
     </div>
