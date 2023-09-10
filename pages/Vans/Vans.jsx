08d94c72bd1211495b734de82defdd2b1029c320
bruxo <diogo.stein@outlook.com.br>
2023-09-10 16:58:30 -0300

    up to lesson 52

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -36,6 +36,32 @@ function Vans() {
   return (
     <div className="van-list-container wrapper">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <button
+          onClick={() => setSearchParams({ type: "simple" })}
+          className="van-type simple"
+        >
+          Simple
+        </button>
+        <button
+          onClick={() => setSearchParams({ type: "luxury" })}
+          className="van-type luxury"
+        >
+          Luxury
+        </button>
+        <button
+          onClick={() => setSearchParams({ type: "rugged" })}
+          className="van-type rugged"
+        >
+          Rugged
+        </button>
+        <button
+          onClick={() => setSearchParams({})}
+          className="van-type clear-filters"
+        >
+          Clear
+        </button>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
